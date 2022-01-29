ccca564e72869ce9d4ec8cb9375b722d46e1c6d2
Ian Friedrichs <ifriedri@stevens.edu>
2022-01-29 17:00:24 -0500

    Compiled hello world

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gfortran -std=f95 -Wextra -Wall -pedantic mainprog.f95 -o mainprog
---
+++ mainprog.f95
@@ -0,0 +1,7 @@
+program main_prog
+
+    implicit none
+
+    print*, "hello world"
+
+end program main_prog
